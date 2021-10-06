f37ba21eeb42e0de404ea7a9fd00a24f478653c1
Riaz <78094939+rihus@users.noreply.github.com>
2021-10-06 11:11:07 +0200

    Basics of Python

---
+++ strings.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 6 10:33:22 2021
+
+@author: riaz
+
+"""
+
+## A string is a series of characters. Anything inside a single quote ''
+## or double quote is a string "". Few ways to use this flexibility:
+
+string_one = 'I told my friend: "Come on!"'
+string_two = "\nThe language 'Python' is named after Monty Python, not the snake."
+string_three = "\nIn Python, \none's use of apostrophe works too"
+
+print(string_one, string_two, string_three)
+
+## As we saw we need to used different quotes in a single string.
+## We can print multiple strings in same print command
+## \n for new line, \t for tab, works in strings
+
+## Changing case in a string
+name = "jack reacher"
+print("this is lower case name: " + name)
+print("this is title case name: " +name.title())
+print("this is upper case name: " +name.upper())
+print("\tthis is again lower case name with a tab at start: " +name.lower())
+## Clearly, print can combine two strings (and string with variables)
+## This is called "concatenation"
+print(string_one + " " + string_two)
+##also
+print("Title case string one follows: " + string_one.title())
+
+## string.rstrip() is used to strip white spaces from the right end of a string
+## string.rstrip() is used to strip white spaces from the left end of a string
+## string.strip() is used to strip white spaces from both ends of a string
+
+## These things are useful when we are taking user input in some program
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 6 10:13:31 2021
+
+@author: riaz
+
+Guidelines for variable names:
+    i) Only letters, numbers and underscores are allowed in variable names
+    ii) Spaces are not allowed (use underscores instead)
+    iii) Don't use python keywords and functions
+    iv) Always keep variable names short but descriptive
+
+"""
+
+### Basics of Python!
+print("Hello World!")
+
+## Veriable assignment in Python
+message = "This text belongs to message variable!"
+print(message)
+
+##if you use same variable name, python still works
+message = "This is for 2nd message string!"
+print(message)
+
+##Now variable 'message' contains the last string we stored. Let's check
+print(message)
