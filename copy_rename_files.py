086bfa4218d9299a9df489710c2bbbf4d1b304e4
Riaz <78094939+rihus@users.noreply.github.com>
2023-04-20 12:37:55 -0400

    added a new renaming code

--- copy_rename_files.py
+++ copy_rename_files.py
@@ -25,7 +25,6 @@ for root, dirs, files in os.walk(source_directory):
 
 
 #%%Renaming files in a prticular directory
-
 source_directory = r'C:\Users\HUSDQ4\OneDrive - cchmc\cincy_work\human_data\VDP_analysis\ILD_HCs'
 file_to_rename = 'img_ventilation_corrected.nii.gz'
 
@@ -50,9 +49,60 @@ for root, dirs, files in os.walk(source_directory):
             # Copy the file to the destination with the new name
             os.rename(source_file, destination_file)
 
+#%%Another code for renaming multiple files in multiple directories
+source_directory = r'C:\Users\HUSDQ4\OneDrive - cchmc\cincy_work\human_data\VDP_analysis\ILD_HCs'
+#Indentify files to be renamed
+file_name_orig = r'^ILD-HC-\d{3}_Vent_\d{8}\.nii\.gz$'
+file_name_N4 = r'^ILD-HC-\d{3}_Vent_\d{8}N4\.nii\.gz$'
+file_name_FAkey = r'^ILD-HC-\d{3}_Vent_\d{8}_corrected\.nii\.gz$'
+file_name_mask = r'^ILD-HC-\d{3}_Vent_\d{8}_mask\.nii\.gz$'
+
+#New file names
+new_file_name_orig = 'img_ventilation.nii.gz'
+new_file_name_N4 = 'img_ventilation_N4.nii.gz'
+new_file_name_FAkey = 'img_ventilation_corrected.nii.gz'
+new_file_name_mask = 'img_ventilation_mask.nii.gz'
+
+# Loop through all directories and files in the source directory
+for root, dirs, files in os.walk(source_directory):
+    for file in files:
+        # Check if the file name matches the original file name pattern
+        if re.match(file_name_orig, file):
+            # Construct the new file name by replacing the original file name with the new file name
+            new_file1 = re.sub(file_name_orig, new_file_name_orig, file)
+            # Construct the full file paths
+            old_file_path1 = os.path.join(root, file)
+            new_file_path1 = os.path.join(root, new_file1)
+            print(f'Renamed file: \n {old_file_path1} -> {new_file_path1}')
+            # Rename the file
+            os.rename(old_file_path1, new_file_path1)
+        elif re.match(file_name_N4, file):
+            # Construct the new file name by replacing the original file name with the new file name
+            new_file2 = re.sub(file_name_N4, new_file_name_N4, file)
+            # Construct the full file paths
+            old_file_path2 = os.path.join(root, file)
+            new_file_path2 = os.path.join(root, new_file2)
+            print(f'Renamed file: \n {old_file_path2} -> {new_file_path2}')
+            # Rename the file
+            os.rename(old_file_path2, new_file_path2)
+        elif re.match(file_name_FAkey, file):
+            # Construct the new file name by replacing the original file name with the new file name
+            new_file3 = re.sub(file_name_FAkey, new_file_name_FAkey, file)
+            # Construct the full file paths
+            old_file_path3 = os.path.join(root, file)
+            new_file_path3 = os.path.join(root, new_file3)
+            print(f'Renamed file: \n {old_file_path3} -> {new_file_path3}')
+            # Rename the file
+            os.rename(old_file_path3, new_file_path3)
+        elif re.match(file_name_mask, file):
+            # Construct the new file name by replacing the original file name with the new file name
+            new_file4 = re.sub(file_name_mask, new_file_name_mask, file)
+            # Construct the full file paths
+            old_file_path4 = os.path.join(root, file)
+            new_file_path4 = os.path.join(root, new_file4)
+            print(f'Renamed file: \n {old_file_path4} -> {new_file_path4}')
+            # Rename the file
+            os.rename(old_file_path4, new_file_path4)
+
+
 #%%
-
-
-
-
-
